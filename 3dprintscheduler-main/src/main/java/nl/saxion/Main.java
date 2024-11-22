0bbf4243a67b4fc381fa2e480bcf4e7231fb6013
damien <537949@student.saxion.nl>
2024-11-22 12:04:58 +0100

    classDiagram added

--- 3dprintscheduler-main/src/main/java/nl/saxion/Main.java
+++ 3dprintscheduler-main/src/main/java/nl/saxion/Main.java
@@ -5,7 +5,6 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-
 import javax.print.DocFlavor;
 import java.io.FileReader;
 import java.io.FileWriter;
