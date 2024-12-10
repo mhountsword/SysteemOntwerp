61e8d6a476fff1674b6d136edc00d82eee383117
damien <537949@student.saxion.nl>
2024-12-10 13:22:38 +0100

    verbeteringen

---
+++ 3dprintscheduler-main/src/main/java/nl/saxion/newModels/PrinterFactory.java
@@ -0,0 +1,16 @@
+package nl.saxion.newModels;
+
+public class PrinterFactory {
+    
+    public void addPrinter(String[] values){
+        if (values[1].equals("1")){
+            
+        } else if (values[1].equals("2")) {
+            
+        } else if (values[1].equals("3")) {
+            
+        } else if (values[1].equals("4")) {
+            
+        }
+    }
+}
