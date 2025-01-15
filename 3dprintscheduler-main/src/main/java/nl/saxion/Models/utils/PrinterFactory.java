5a5d162add30149665ef97416abd92b9d849aa33
damien <537949@student.saxion.nl>
2025-01-15 14:09:17 +0100

    commit

--- 3dprintscheduler-main/src/main/java/nl/saxion/Models/utils/PrinterFactory.java
+++ 3dprintscheduler-main/src/main/java/nl/saxion/Models/utils/PrinterFactory.java
@@ -1,8 +1,10 @@
 package nl.saxion.Models.utils;
+
 import nl.saxion.Models.printers.HousedPrinter;
 import nl.saxion.Models.printers.MultiColor;
 import nl.saxion.Models.printers.Printer;
 import nl.saxion.Models.printers.StandardFDM;
+
 public class PrinterFactory {
 
     public PrinterFactory() {
@@ -11,10 +13,10 @@ public class PrinterFactory {
     public Printer createPrinterByType(int type, int id, String name, String manufacturer, int maxX, int maxY, int maxZ, int maxColors) {
         return switch (type) {
             case 1 -> new StandardFDM(id, name, manufacturer, maxX, maxY, maxZ, false);
-            case 2 -> new HousedPrinter(id, name, manufacturer, maxX, maxY, maxZ, true);
-            default -> {
-                yield new MultiColor(id, name, manufacturer, maxX, maxY, maxZ, maxColors);
-            }
+            case 2 ->
+                    new HousedPrinter(id, name, manufacturer, maxX, maxY, maxZ);// new StandardFDM(id, name, manufacturer, maxX, maxY, maxZ, true);
+
+            default -> new MultiColor(id, name, manufacturer, maxX, maxY, maxZ, maxColors);
         };
     }
 }
