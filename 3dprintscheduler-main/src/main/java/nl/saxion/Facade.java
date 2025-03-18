6224b72a51572e1d9e37b46a93fdc90002ff6412
damien <537949@student.saxion.nl>
2025-03-18 15:01:24 +0100

    facade gefixed

--- 3dprintscheduler-main/src/main/java/nl/saxion/Facade.java
+++ 3dprintscheduler-main/src/main/java/nl/saxion/Facade.java
@@ -1,14 +1,19 @@
 package nl.saxion;
 
 import nl.saxion.Models.managers.*;
+import nl.saxion.Models.observer.Observer;
+import nl.saxion.Models.observer.Updater;
 import nl.saxion.Models.printers.*;
 import nl.saxion.Models.prints.Print;
 import nl.saxion.Models.spools.Spool;
 import nl.saxion.exceptions.PrintError;
 import nl.saxion.utils.readers.Reader;
+
 import java.util.*;
 
-public class Facade {
+public class Facade implements Updater {
+    private int spoolchanges = 0;
+    private int totalprints = 0;
     private final PrinterManager printerManager;
     private final PrintManager printManager;
     private final PrintTaskManager printTaskManager;
@@ -17,11 +22,14 @@ public class Facade {
     private final Scanner scanner = new Scanner(System.in);
     private static Facade instance;
 
+    private final Observer observer;
+
     private static final int TYPE_UNKNOWN = 0;
     private static final int TYPE_STANDARD_FDM = 1;
     private static final int TYPE_HOUSED_MULTICOLOR = 2;
     private static final int TYPE_MULTICOLOR = 3;
     private static final int TYPE_HOUSED = 4;
+
 
     private Facade() {
         printerManager = PrinterManager.getInstance();
@@ -29,15 +37,18 @@ public class Facade {
         printTaskManager = PrintTaskManager.getInstance();
         spoolManager = SpoolManager.getInstance();
         strategyManager = StrategyManager.getInstance();
+        observer = Observer.getInstance();
+        observer.subscribe(this);
+
 
         Reader fileReader = new Reader();
         List<Print> prints = fileReader.readPrintsFromFile("prints.json");
-        List<Spool> spools = fileReader.readSpoolsFromFile("src/main/resources/spools.csv");
+        List<Spool> spools = fileReader.readSpoolsFromFile("spools.json");
         List<Printer> printers = fileReader.readPrintersFromFile("printers.json");
 
         spools.forEach(spoolManager::addSpool);
         prints.forEach(printManager::addPrint);
-        for(Printer printer : printers) {
+        for (Printer printer : printers) {
             printerManager.addPrinter(
                     printer.getId(),
                     calculatePrinterTypeCategory(printer),
@@ -62,7 +73,7 @@ public class Facade {
         int printerId = scanner.nextInt();
         try {
             printTaskManager.registerPrintCompletion(printerId);
-        }catch (PrintError e){
+        } catch (PrintError e) {
             System.out.println(e.getMessage());
         }
     }
@@ -93,6 +104,14 @@ public class Facade {
         spoolManager.printSpools();
     }
 
+    public int getSpoolchanges() {
+        return spoolchanges;
+    }
+
+    public int getTotalprints() {
+        return totalprints;
+    }
+
     private int calculatePrinterTypeCategory(Printer printer) {
         if (printer instanceof StandardFDM) {
             return TYPE_STANDARD_FDM;
@@ -116,5 +135,24 @@ public class Facade {
             instance = new Facade();
         }
         return instance;
+    }
+
+    public void showstats() {
+        System.out.println("---------- Statisctics ----------");
+        System.out.println("Spool changes: " + getSpoolchanges());
+        System.out.println("totalprints prints: " + getTotalprints());
+        System.out.println("-------------------------------------------");
+
+    }
+
+    public void exit(){
+        observer.unsubscribe(instance);
+        System.out.println("goodbye");
+    }
+
+    @Override
+    public void update(int spoolchanges, int totalprints) {
+        this.spoolchanges = spoolchanges;
+        this.totalprints = totalprints;
     }
 }
